8e7f9857b2bd601f6bc67510b7b2ffda655b5648
rafa.santos <rafael@magazineluiza.com.br>
2026-01-26 19:56:16 -0300

    4.8: generating and executing stub changed files

--- src/contractTest/resources/contracts/product/findProductByIdV1.groovy
+++ src/contractTest/resources/contracts/product/findProductByIdV1.groovy
@@ -14,18 +14,21 @@ Contract.make {
         status 200
         headers {
             contentType 'application/json'
-            body([
-                    id: fromRequest().path(3),
-                    addedAt: anyIso8601WithOffset(),
-                    name: "Notebook X11",
-                    brand: "Deep Diver",
-                    regularPrice: 1500.00,
-                    salePrice: 1000.00,
-                    inStock: false,
-                    enabled: true,
-                    categoryId: anyUuid(),
-                    description: "A Gamer Notebook"
-            ])
         }
+        body([
+                id: fromRequest().path(3),
+                addedAt: anyIso8601WithOffset(),
+                name: "Notebook X11",
+                brand: "Deep Diver",
+                regularPrice: 1500.00,
+                salePrice: 1000.00,
+                inStock: true,
+                enabled: true,
+                category: [
+                        id: anyUuid(),
+                        name: "Notebook"
+                ],
+                description: "A Gamer Notebook"
+        ])
     }
 }
