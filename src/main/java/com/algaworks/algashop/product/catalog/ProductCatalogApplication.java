5868a6ade072c765fcf1ac4adcb54703a2f5298c
rafa.santos <rafael@magazineluiza.com.br>
2026-03-22 12:45:47 -0300

    5.12: Enhancing Customer API responses

--- src/main/java/com/algaworks/algashop/product/catalog/ProductCatalogApplication.java
+++ src/main/java/com/algaworks/algashop/product/catalog/ProductCatalogApplication.java
@@ -1,5 +1,7 @@
 package com.algaworks.algashop.product.catalog;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductCatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+      SpringApplication.run(ProductCatalogApplication.class, args);
 	}
 
 }
