1042544e50b94311200eedd7de4cbf11cc19f206
rafa.santos <rafael@magazineluiza.com.br>
2026-08-13 08:24:42 -0300

    2.8: Implementing a date converter

--- src/main/java/com/algaworks/algashop/product/catalog/domain/model/category/Category.java
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/category/Category.java
@@ -27,15 +27,15 @@ public class Category {
 
   private Boolean enabled;
 
-//  private OffsetDateTime createdAt;
+  private OffsetDateTime createdAt;
 
-//  private OffsetDateTime updatedAt;
+  private OffsetDateTime updatedAt;
 
   public Category(String name, Boolean enabled) {
     this.id = IdGenerator.generateTimeBasedUUID();
     this.setName(name);
     this.setEnabled(enabled);
-    //this.createdAt = OffsetDateTime.now();
+    this.createdAt = OffsetDateTime.now();
   }
 
   public void setName(String name) {
--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/MongoConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/MongoConfig.java
@@ -1,9 +1,17 @@
 package com.algaworks.algashop.product.catalog.infrastructure;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+
 import org.bson.UuidRepresentation;
+import org.jspecify.annotations.Nullable;
 import org.springframework.boot.mongodb.autoconfigure.MongoClientSettingsBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 
 @Configuration
 public class MongoConfig {
@@ -12,5 +20,27 @@ public class MongoConfig {
   public MongoClientSettingsBuilderCustomizer uuidCustomizer() {
     return builder -> builder.uuidRepresentation(UuidRepresentation.STANDARD);
   }
+
+  @Bean
+  public MongoCustomConversions customConversions() {
+    return new MongoCustomConversions(
+        List.of(new OffsetDateTimeConverter(), new OffsetDateTimeWriteConverter()));
+  }
+
+  public static class OffsetDateTimeConverter implements Converter<Date, OffsetDateTime> {
+    @Override
+    public @Nullable OffsetDateTime convert(Date source) {
+      return source.toInstant().atZone(ZoneId.systemDefault()).toOffsetDateTime();
+    }
+  }
+
+  public static class OffsetDateTimeWriteConverter implements Converter<OffsetDateTime, Date> {
+    @Override
+    public @Nullable Date convert(OffsetDateTime source) {
+      return Date.from(source.toInstant());
+    }
+  }
+
+
 
 }
