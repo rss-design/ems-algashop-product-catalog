444278a9a4f9959028a33e7431ad36adbfa036df
rafa.santos <rafael@magazineluiza.com.br>
2026-08-17 08:45:26 -0300

    2.10: Mapping Product to Document

---
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/Product.java
@@ -0,0 +1,70 @@
+package com.algaworks.algashop.product.catalog.domain.model.product;
+
+import com.algaworks.algashop.product.catalog.domain.model.IdGenerator;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String name;
+
+    private String brand;
+
+    private String description;
+
+    private Integer quantityInStock;
+
+    private Boolean enabled;
+
+    private BigDecimal regularPrice;
+
+    private BigDecimal salePrice;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    private OffsetDateTime updatedAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
+
+    @Builder
+    public Product(String name, String brand, String description,
+                   Boolean enabled, BigDecimal regularPrice, BigDecimal salePrice) {
+        this.id = IdGenerator.generateTimeBasedUUID();
+        this.name = name;
+        this.brand = brand;
+        this.description = description;
+        this.enabled = enabled;
+        this.regularPrice = regularPrice;
+        this.salePrice = salePrice;
+    }
+}
